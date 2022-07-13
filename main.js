b99ab910366a4773a0a9b50192d05e54cd377673
FirehiwotM <107230305+FirehiwotM@users.noreply.github.com>
2022-07-13 10:49:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-let day='Monday, Tuesday, wednesday,Thursday,Friday, Saturday';
+let day = "Saturday";
 
-console.log(day);
+day = day.toLocaleLowerCase();
+let dayToSat;
+
+switch (day) {
+    case 'Sunday':
+        dayToSat = 6;
+        break;
+    case 'Monday':
+        dayToSat = 5;
+        break;
+    case 'Tuesday':
+        dayToSat = 4;
+        break;
+    case 'Wednesday':
+        dayToSat = 3;
+        break;
+    case 'Thursday':
+        dayToSat = 2;
+        break;
+    case 'Friday':
+        dayToSat = 1;
+        break;
+    case 'Saturday':
+        dayToSat = 0;
+        break;
+        default:
+            dayToSat = null;
+            break;
+
+
+}
